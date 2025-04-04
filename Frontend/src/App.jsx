377c38c66af53555a5a46e1b40b938ce5cfc3190
Dave7103 <122285015+Dave7103@users.noreply.github.com>
2025-04-04 23:01:02 +0800

    last commit?

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import TodoList from "./TodoList";
+
+
+function App() {
+    return (
+    <div>
+        <h1> React TODO App</h1>
+        <TodoList />
+  </div>
+  
+);
+}
+
+export default App;
+
