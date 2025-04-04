879ccb9bc7dffc795e83d68bc72d019ecd188095
Dave7103 <122285015+Dave7103@users.noreply.github.com>
2025-04-04 23:22:19 +0800

    s

--- todo_list/Backend/todo/views.py
+++ todo_list/Backend/todo/views.py
@@ -1,22 +1,8 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import viewsets
 from .models import Post
 from .serializers import PostSerializer
-from django.http import JsonResponse
 
-@api_view(['GET', 'POST'])
-def post_list(request):
-    if request.method == 'GET':
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-    return Response(serializer.errors, status=400)
-
-def api_data(request):
-    return JsonResponse({"message": "Hello from Django!"})
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
