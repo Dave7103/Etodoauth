1411577dff8feba4665c145bac2dd9f15c590be4
Dave7103 <122285015+Dave7103@users.noreply.github.com>
2025-04-04 23:23:19 +0800

    s

--- todo_list/Backend/todolist/urls.py
+++ todo_list/Backend/todolist/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from todo.views import post_list
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from todo.views import PostViewSet
+
+router = DefaultRouter()
+router.register(r'post', PostViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/posts/', post_list, name='post_list'),
+    path('api/', include(router.urls)),
 ]
