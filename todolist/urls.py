5edd5be9a03511b2b16f5cb62deedd805e0495db
Dave7103 <122285015+Dave7103@users.noreply.github.com>
2025-04-17 21:10:06 +0800

    s

--- todolist/urls.py
+++ todolist/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api-token-auth/', obtain_auth_token),
-    path('', include(todo.urls)),
+    path('', include('todo.urls')),
 ]
