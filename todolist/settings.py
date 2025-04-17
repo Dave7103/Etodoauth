b727251baa515117cdd6bf311d8c553abd2441f4
Dave7103 <122285015+Dave7103@users.noreply.github.com>
2025-04-17 20:22:23 +0800

    s

--- todolist/settings.py
+++ todolist/settings.py
@@ -57,8 +57,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'corsheaders',
     'todo',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
